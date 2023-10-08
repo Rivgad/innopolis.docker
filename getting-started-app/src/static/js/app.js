e6337e949880eba37f58e06e1f6e0dce440cea67
Yusupov Adel <losvermo@gmail.com>
2023-10-09 00:42:25 +0300

    update app.js (#2)

--- getting-started-app/src/static/js/app.js
+++ getting-started-app/src/static/js/app.js
@@ -1,4 +1,4 @@
-function App() {
+    function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
         <Container>
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">You have no todo items yet! Add one above!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
